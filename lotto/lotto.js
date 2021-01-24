83c4619086dfd3f7fe7d5d2eb6b57bd065341a00
KimMinSoo <starirof@naver.com>
2021-01-24 22:02:10 +0900

    로또 생성기 번호 화면에 표시

--- lotto/lotto.js
+++ lotto/lotto.js
@@ -1,3 +1,6 @@
+// 전역.
+let nthTry = 1;
+
 function makeLottoNumbers() {
 
     let numberSet = new Set();
@@ -8,4 +11,35 @@ function makeLottoNumbers() {
         console.log(number);
     }
 
+    outputLottoNumbers(numberSet);
+}
+
+// 로또 번호를 화면에 표시.
+function outputLottoNumbers(numberSet) {
+    let numbersElem = '';
+    numbersElem += "<span>" + nthTry + "번째 생성 : </span>";
+
+    let i = 1;
+    for (let number of numberSet) {
+        numbersElem += "<span class='lotto-num' id='num" + i + "'> " + number + "</span>";
+        i++;
+    }
+
+    const NOW_DATE = makeDate();
+    numbersElem += ' 생성시간 : ' + NOW_DATE;
+    numbersElem += '<br>';
+    document.getElementById('lotto-num-div').insertAdjacentHTML('afterbegin',numbersElem);
+    nthTry++;
+}
+
+// 생성날짜를 생성.
+function makeDate() {
+    const now = new Date().toLocaleString();
+
+    return now;
+}
+
+// 로또 생성 필드를 초기화.
+function resetLotto() {
+    document.getElementById('lotto-num-div').innerHTML = '';
 }
