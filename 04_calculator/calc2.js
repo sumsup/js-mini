afefd0ee31bf431775e49fddcda700ff13bf06bc
KimMinSoo <starirof@naver.com>
2021-02-21 16:15:13 +0900

    - 계산기 버그. if문 떡칠해서 고치긴 고침... 이후에 로직 단순화를 통해서 직관적인 코드로 개선하기.

--- 04_calculator/calc2.js
+++ 04_calculator/calc2.js
@@ -3,10 +3,23 @@
 // - 숫자 버튼 조합으로 계산 가능하게.
 // - 키보드로 계산 가능하게.
 
+// TODO
+// 문제점.
+// 처리 로직이 여기저기 분산되 있고.
+// flag도 하나의 목적으로 사용해야 하는데 여러가지 목적으로 사용하다보니까
+// flag를 변경하면 의도치 않은 부작용들이 발생한다.
+// 하나의 flag는 하나의 책임을 지도록 하자.
+// 함수도 마찬가지.
+// 단일책임의 원칙이 여기서 나왔구나.
+// - 단일책임의 원칙. 하나의 flag는 하나의 목적으로.
+// - 분산된 로직을 하나로 모으기.
+
 let inputNum;
 let outputStr = '';
-let mark;
+let outputMark; // 화면에 표시용 기호.
+let calcMark; // 실제 계산용 기호.
 let resultNum = new Number();
+let isDoEqualFlag = false; // = 을 한번이라도 실행했는지 여부를 체크.
 
 window.onload = function() {
     eventListeners();
@@ -22,17 +35,26 @@ function eventListeners() {
     for (let i = 0; i < calcBtns.length; i++) {
         calcBtns[i].addEventListener('click', function () {
             inputNum = Number(document.querySelector('#inputNum').value);
-            mark = event.target.innerHTML;
+            outputMark = event.target.innerHTML;
+
+            // 최초에 계산용 기호에 할당.
+            // = 을 실행한 이후에는 즉시 할당.
+            // 이후부터는, 한번 계산이 끝나면 doCalc()에서 변경해줌.
+            if (calcMark === undefined || isDoEqualFlag === true) {
+                calcMark = outputMark;
+            }
 
             if (document.querySelector('#inputNum').value === '') {
                 alert('숫자를 입력해 주세요!');
+                document.getElementById('inputNum').focus();
                 return;
             }
 
             let isIncludeEqual = outputStr.includes('=') ? true : false;
             // 계산도 해야지.
             if (isIncludeEqual) {
-                outputStr = resultNum + ' ' + mark + ' ';
+                outputStr = resultNum + ' ' + outputMark + ' ';
+                calcMark = outputMark;
                 doCalc();
             }
             else if (outputStr !== '') {
@@ -42,14 +64,28 @@ function eventListeners() {
                 resultNum = inputNum;
             }
 
-            outputStr += inputNum;
-            // 앞의 계산식이 + 나 - 일 경우.
-            if (outputStr.length >= 4 && (mark === '×' || mark === '÷')) {
-                outputStr = '(' + outputStr + ')';
+            // 앞의 계산식이 + 나 - 이고 뒤에 곱셈이나 나눗셈을 할때 앞 숫자들에 괄호를 씌워줘야 함.
+            if (outputStr.length >= 4 && (outputMark === '×' || outputMark === '÷')) {
+                if (isDoEqualFlag === true) {
+                    if (isIncludeEqual === false) {
+                        outputStr += outputMark + ' ';
+                    }
+                    outputStr += inputNum;
+                    outputStr = '(' + outputStr + ') ';
+                }
+                else if (isDoEqualFlag === false) {
+                    outputStr += inputNum;
+                    outputStr = '(' + outputStr + ') ';
+                    outputStr += outputMark + ' ';
+                }
             }
-            if (!isIncludeEqual) {
-                outputStr += ' ' + mark + ' ';
+            else if (!isIncludeEqual && isDoEqualFlag === false) {
+                outputStr += inputNum + ' ' + outputMark + ' ';
             }
+            else if (!isIncludeEqual && isDoEqualFlag === true) {
+                outputStr += ' ' + outputMark + ' ' + inputNum;
+            }
+
             outputStrElem.innerHTML = outputStr;
             equalElem.disabled = false;
             inputElem.value = '';
@@ -63,13 +99,14 @@ function eventListeners() {
 
         if (outputStr !== '') {
             doCalc();
-            mark = '=';
+            outputMark = '=';
         }
 
         outputStr += inputNum + ' = ' + resultNum;
         outputStrElem.innerHTML = outputStr;
         inputElem.value = '';
         inputElem.focus();
+        isDoEqualFlag = true;
     });
 
     // c 버튼을 눌렀을 때. 리셋.
@@ -77,7 +114,9 @@ function eventListeners() {
         inputNum = new Number();
         resultNum = new Number();
         outputStr = '';
-        mark = '';
+        outputMark = undefined;
+        calcMark = undefined;
+        isDoEqualFlag = false;
         outputStrElem.innerHTML = '';
         inputElem.value = '';
         inputElem.focus();
@@ -88,17 +127,18 @@ function doCalc() {
     resultNum =  Number(resultNum);
     if (inputNum !== '') {
         inputNum = Number(inputNum);
-        if (mark === '+') {
+        if (calcMark === '+') {
             resultNum += inputNum;
         }
-        else if (mark === '-') {
+        else if (calcMark === '-') {
             resultNum -= inputNum;
         }
-        else if (mark === '×') {
+        else if (calcMark === '×') {
             resultNum *= inputNum;
         }
-        else if (mark === '÷') {
+        else if (calcMark === '÷') {
             resultNum /= inputNum;
         }
     }
+    calcMark = outputMark;
 }
